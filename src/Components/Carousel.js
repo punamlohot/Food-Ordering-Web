62d3f02eb57d063beb6593b5b8fcee2d68f74c17
punam lohot <punamlohot4@gmail.com>
2024-04-03 09:59:17 +0530

    added dependencies

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -1,33 +1,48 @@
-import React from 'react';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Import carousel styles
-import { Carousel } from 'react-responsive-carousel';
+import React from "react";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import carousel styles
+import { Carousel } from "react-responsive-carousel";
 
 const CarouselComponent = () => {
-    return (
-        <Carousel
-            showThumbs={false} // Optionally, hide the thumbnail navigation
-            autoPlay={true}    // Auto-play the carousel
-            interval={2000}    // Time between slides in milliseconds
-        >
-
-            <div className="carousel-slide">
-                <img src="https://i1.wp.com/www.techjaja.com/wp-content/uploads/2018/01/Food-Delivery.jpeg?fit=1200%2C760&ssl=1" alt="rest-1" height="500px" />
-                <p className="carousel-text">Lightning-Fast Delivery</p>
-            </div>
-            <div className="carousel-slide">
-                <img src="https://www.fresconews.com/wp-content/uploads/2020/05/images1401-5ec4e2fbe2d26.jpg" alt="rest-2" height="500px" />
-                <p className="carousel-text">Cash On Delivery</p>
-            </div>
-            <div className="carousel-slide">
-                <img src="https://thesmartlocal.com/wp-content/uploads/2020/05/healthy-delivery9.png" alt="rest-2" height="500px" />
-                <p className="carousel-text">No Minimum Order</p>
-            </div>
-            <div className="carousel-slide">
-                <img src="https://www.enukesoftware.com/blog/wp-content/uploads/2017/04/onine-food-delivery.jpg" alt="rest-2" height="500px" />
-                <p className="carousel-text">Food Available At Your Doorstep 24/7 </p>
-            </div>
-        </Carousel>
-    );
+  return (
+    <Carousel
+      showThumbs={false} // Optionally, hide the thumbnail navigation
+      autoPlay={true} // Auto-play the carousel
+      interval={2000} // Time between slides in milliseconds
+    >
+      <div className="carousel-slide">
+        <img
+          src="https://i1.wp.com/www.techjaja.com/wp-content/uploads/2018/01/Food-Delivery.jpeg?fit=1200%2C760&ssl=1"
+          alt="rest-1"
+          height="500px"
+        />
+        <p className="carousel-text">Lightning-Fast Delivery</p>
+      </div>
+      <div className="carousel-slide">
+        <img
+          src="https://www.fresconews.com/wp-content/uploads/2020/05/images1401-5ec4e2fbe2d26.jpg"
+          alt="rest-2"
+          height="500px"
+        />
+        <p className="carousel-text">Cash On Delivery</p>
+      </div>
+      <div className="carousel-slide">
+        <img
+          src="https://thesmartlocal.com/wp-content/uploads/2020/05/healthy-delivery9.png"
+          alt="rest-2"
+          height="500px"
+        />
+        <p className="carousel-text">No Minimum Order</p>
+      </div>
+      <div className="carousel-slide">
+        <img
+          src="https://www.enukesoftware.com/blog/wp-content/uploads/2017/04/onine-food-delivery.jpg"
+          alt="rest-2"
+          height="500px"
+        />
+        <p className="carousel-text">Food Available At Your Doorstep 24/7 </p>
+      </div>
+    </Carousel>
+  );
 };
 
 export default CarouselComponent;
